9c660400d02fddc57ddfb749515763930b94aaeb
divadGD924 <davdiadgd@gmail.com>
2025-08-06 19:21:33 +0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,25 +100,25 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        ne csalj
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        fingtam
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        zsolt egy furry
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        banja golyoi palya glazer kell lenni
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        4
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        kattints
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        4444
                     </p>
                     <p>
                         Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
